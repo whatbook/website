c33cdeaa447ca4e07a96268b17c694d4b50f58f2
Ako520 <jingnanbendan@163.com>
2019-02-10 23:57:42 +0800

    数据加载并简单调整时间

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ Desc: Define inital variables
       stageCenterX = stageWidth / 2,
       stageCenterY = stageHeight / 2,
       countdownFrom = 0,
-      countdownTimer = 2800,
+      countdownTimer = 4000,
       countdownRunning = true,
       breakTimer = 1000,
       number,
@@ -32,7 +32,164 @@ Desc: Define inital variables
         '255, 211, 218',
         '151, 211, 226',
       ]
-    var releases = ['1.0.0', '2.10.0']
+    var releases = [
+      {
+        tag: '2018-4-26今天我们上线啦！',
+        version: '1.0.0',
+      },
+      {
+        tag: '修复一个小问题',
+        version: '1.0.1',
+      },
+      {
+        tag: '1.1.0 你没有用过的船新版本',
+        version: '1.1.0',
+      },
+      {
+        tag: '修复问题',
+        version: '1.1.1',
+      },
+      {
+        tag: '增加临时的图片旋转解决方案',
+        version: '1.1.2',
+      },
+      {
+        tag: '优化图片预览是显示图片速度',
+        version: '1.1.3',
+      },
+      {
+        tag: '1.2.0',
+        version: '1.2.0',
+      },
+      {
+        tag: '1.2.1',
+        version: '1.2.1',
+      },
+      {
+        tag: '1.2.2',
+        version: '1.2.2',
+      },
+      {
+        tag: '1.2.3',
+        version: '1.2.3',
+      },
+      {
+        tag: '1.2.4',
+        version: '1.2.4',
+      },
+      {
+        tag: '1.3.0',
+        version: '1.3.0',
+      },
+      {
+        tag: '1.3.1',
+        version: '1.3.1',
+      },
+      {
+        tag: '修复第一次编辑简要病史保存后不收起不回显的问题',
+        version: '1.3.2',
+      },
+      {
+        tag: '1.3.3',
+        version: '1.3.3',
+      },
+      {
+        tag: '抓紧！2.0发车了！',
+        version: '2.0.0',
+      },
+      {
+        tag: '2.1.0',
+        version: '2.1.0',
+      },
+      {
+        tag: '2.2.0',
+        version: '2.2.0',
+      },
+      {
+        tag: '2.2.1',
+        version: '2.2.1',
+      },
+      {
+        tag: '2.2.2',
+        version: '2.2.2',
+      },
+      {
+        tag: '2.2.3',
+        version: '2.2.3',
+      },
+      {
+        tag: '2.2.4',
+        version: '2.2.4',
+      },
+      {
+        tag: '2.3.0',
+        version: '2.3.0',
+      },
+      {
+        tag: '湘雅-2.4.0',
+        version: '2.4.0',
+      },
+      {
+        tag: '2.4.1',
+        version: '2.4.1',
+      },
+      {
+        tag: '2.5.0',
+        version: '2.5.0',
+      },
+      {
+        tag: '2.6.0',
+        version: '2.6.0',
+      },
+      {
+        tag: '2.7.0',
+        version: '2.7.0',
+      },
+      {
+        tag: '2.8.0',
+        version: '2.8.0',
+      },
+      {
+        tag: '2.8.1',
+        version: '2.8.1',
+      },
+      {
+        tag: '2.9.0',
+        version: '2.9.0',
+      },
+      {
+        tag: '2.9.1',
+        version: '2.9.1',
+      },
+      {
+        tag: '2.9.2',
+        version: '2.9.2',
+      },
+      {
+        tag: '2.9.3',
+        version: '2.9.3',
+      },
+      {
+        tag: '2.10.0',
+        version: '2.10.0',
+      },
+      {
+        tag: '2.11.0',
+        version: '2.11.0',
+      },
+      {
+        tag: '2.11.1',
+        version: '2.11.1',
+      },
+      {
+        tag: '2.11.2',
+        version: '2.11.2',
+      },
+      {
+        tag: 'Feture',
+        version: 'xx.xx.xx',
+      },
+    ]
 
     /*
 Desc: Init canvases & Number text
@@ -101,13 +258,12 @@ Desc: Countdown
 */
     function countdown() {
       // Send number to be drawn
-      drawNumber(releases[countdownFrom].toString())
+      drawTag(releases[countdownFrom].tag.toString())
 
       // When we hit zero stop countdown
       if (countdownFrom + 1 === releases.length) {
         countdownRunning = false
         // Now that countdowns finised show the text Go
-        drawNumber('2.10.0')
       }
 
       // Decrement number down
@@ -133,7 +289,7 @@ Desc: Redraw loops
     /*
 Desc: Draw number
 */
-    function drawNumber(num) {
+    function drawTag(num) {
       // Create a number on a seperate canvas
       // Use a seperate canvas thats smaller so we have less data to loop over when using getImagedata()
 
@@ -142,7 +298,7 @@ Desc: Draw number
 
       numberStageCtx.fillStyle = '#24282f'
       numberStageCtx.textAlign = 'center'
-      numberStageCtx.font = 'bold 318px Lato'
+      numberStageCtx.font = 'bold 88px Lato'
       numberStageCtx.fillText(num, 600, 400)
 
       var ctx = document.getElementById('canvas-number').getContext('2d')
