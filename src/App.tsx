22b57fec0d7c09c32ac2c4d1f2553b6cc9198914
Ako520 <jingnanbendan@163.com>
2019-02-27 14:27:46 +0800

    feat: add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import Summary from './app/pages/summary'
 import './config.js'
+import { CssBaseline } from '@material-ui/core'
+import AppBar from './app/components/AppBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Summary />
+        <CssBaseline />
+        <AppBar />
+        {/* <Button>
+          你好, 世界
+        </Button>
+        <Summary /> */}
+
       </div>
     )
   }
