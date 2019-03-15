0995c5e403debe8a0159c2366a97173e09dc6a5d
Ako520 <jingnanbendan@163.com>
2019-03-15 08:26:19 +0800

    fix: 修复require.context在测试中找不到的问题

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,11 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
+import requireContext from 'require-context.macro'
 import '../src/index.css'
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src', true, /.stories.js$/);
+const req = requireContext('../src', true, /.stories.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)
