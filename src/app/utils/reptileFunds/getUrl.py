0b3140f3c5cb85982a2d9b25c27928964ece2a85
Ako520 <jingnanbendan@163.com>
2019-03-29 14:18:14 +0800

    fix: 完善对于天天基金网的爬取
    对于未上线的基金  类型二的基金进行不足 并且可以自动切换代理ip

--- src/app/utils/reptileFunds/getUrl.py
+++ src/app/utils/reptileFunds/getUrl.py
@@ -15,6 +15,7 @@ UA_LIST = [
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
     "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
 ]
+
 header = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -28,13 +29,51 @@ header = {
     random.choice(UA_LIST)
 }
 
+
+def random_header():
+    global header
+    header = {
+        'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding':
+        'gzip, deflate, sdch',
+        'Accept-Language':
+        'zh-CN,zh;q=0.8,en;q=0.6',
+        'Connection':
+        'keep-alive',
+        'User-Agent':
+        random.choice(UA_LIST)
+    }
+
+
 proxies = [
-    'http://124.192.106.247:3128',
+    # 'http://117.158.57.2:3128',  # 太慢了
+    # 'http://119.90.126.106:7777',  # 太慢了
+    # 'http://119.57.108.53:53281',  # 太慢了
+    # 'http://163.125.69.140:8888',
+    # 'http://124.192.106.247:3128',
+    # 'http://27.191.234.69:9999',
+    # 'http://125.46.0.62:53281',
+    # 'http://120.237.14.198:53281',
+    # 'http://163.125.69.149:8888',
+
+    'http://27.46.23.52:8888',
+    'http://117.141.155.243:53281',
+    'http://113.78.255.169:9000',
+    'http://180.116.58.224:9999',
+
+    # 'http://122.136.212.132:53281',
+    # 'http://59.44.247.194:9797',
+    # 'http://113.116.58.253:9000'
+
+
+
+
+
+
 ]
 
 random_proxy = random.choice(proxies)
-
-print(random_proxy)
 
 
 def geturl_gbk_common(url):
@@ -47,18 +86,30 @@ def geturl_gbk(url):
     html = requests.get(
         url, headers=header, proxies={
             'http': random_proxy
-        }).content.decode('gbk')
-    soup = BeautifulSoup(html, 'lxml')
+        }, timeout=7).content.decode('gbk')
+    soup = BeautifulSoup(html, 'html.parser')
     return soup
 
 
 def geturl_utf8(url):
+    print('ip', random_proxy)
     html = requests.get(
         url, headers=header, proxies={
             'http': random_proxy
-        }).content.decode('utf-8')
-    soup = BeautifulSoup(html, 'lxml')
+        }, timeout=7).content.decode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
     return soup
+
+
+def loop_geturl_utf8(url):
+    try:
+        return geturl_utf8(url)
+    except:
+        print('重试')
+        global random_proxy
+        random_proxy = random.choice(proxies)
+        random_header()
+        return loop_geturl_utf8(url)
 
 
 # proxies={'http':random.choice(proxies)}
--- src/app/utils/reptileFunds/main.py
+++ src/app/utils/reptileFunds/main.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 import getUrl
-from multiprocessing import Pool
 import requests
 import re
 import pymongo
@@ -14,34 +13,29 @@ clients = pymongo.MongoClient('127.0.0.1')
 db = clients['funds']
 col1 = db['fund']
 col2 = db['detail']
+# soup = getUrl.geturl_gbk(url)
 
-soup = getUrl.geturl_gbk(url)
 
-
-def run_detail2(code, name, url):
-    soup = getUrl.geturl_utf8(url)
-    tags = soup.select('div.dataOfFund dd')
-    m1 = tags[4].select('span')[1].text
-    y1 = tags[5].select('span')[1].text
-    m3 = tags[6].select('span')[1].text
-    y3 = tags[7].select('span')[1].text
-    m6 = tags[8].select('span')[1].text
-    rece = tags[9].select('span')[1].text
+def run_detail3(code, name, url, soup):
     infos = soup.select('div.infoOfFund td')
-    fund_type = infos[0].a.text
+    if infos[0].a is None:
+        fund_type = re.findall(r'基金类型：(.+)\xa0\xa0\|', infos[0].text)
+    else:
+        fund_type = infos[0].a.text
     fund_risk = '无' if len(infos[0].text.split(
         '|\xa0\xa0')) <= 1 else infos[0].text.split('|\xa0\xa0')[1]
     scale_string = infos[1].text.split('：')[1]
     tmp1 = scale_string.split('（')  # ["40.60亿元", "2018-12-31）"] 这种东西
     fund_unit = re.findall(r'(亿元|万元)', tmp1[0])[0]
     fund_scale_lasttime = tmp1[1].split('）')[0]
-    fund_scale = re.findall(r'^([0-9]{1,}[.][0-9]*)', tmp1[0])[0]
+    # 防止出现没有数据 -- 的情形
+    fund_scale = '无' if len(re.findall(
+        r'^([0-9]{1,}[.][0-9]*)', tmp1[0])) == 0 else re.findall(r'^([0-9]{1,}[.][0-9]*)', tmp1[0])[0]
     fund_manager = infos[2].a.text
     fund_birthday = infos[3].text.split('：')[1]
     fund_owener = infos[4].a.text
     fund_rate = infos[5].div.text
-    detail = {'代码': code, '名称': name, '近1月': m1, '近3月': m3,
-              '近6月': m6, '近1年': y1, '近3年': y3, '成立来': rece,
+    detail = {'代码': code, '名称': name,
               '基金类型': fund_type,
               '基金风险': fund_risk, '基金规模': fund_scale, '规模单位': fund_unit,
               '规模最后更新时间': fund_scale_lasttime, '基金经理': fund_manager,
@@ -50,10 +44,50 @@ def run_detail2(code, name, url):
     col2.insert_one(detail)
 
 
-def run_detail1(code, name, url):
-    soup = getUrl.geturl_utf8(url)
+# 类似00009这种的需要使用这种取法
+def run_detail2(code, name, url, soup):
     tags = soup.select('div.dataOfFund dd')
+    try:
+        m1 = tags[4].select('span')[1].text
+        y1 = tags[5].select('span')[1].text
+        m3 = tags[6].select('span')[1].text
+        y3 = tags[7].select('span')[1].text
+        m6 = tags[8].select('span')[1].text
+        rece = tags[9].select('span')[1].text
+        infos = soup.select('div.infoOfFund td')
+        if infos[0].a is None:
+            fund_type = re.findall(r'基金类型：(.+)\xa0\xa0\|', infos[0].text)
+        else:
+            fund_type = infos[0].a.text
+        fund_risk = '无' if len(infos[0].text.split(
+            '|\xa0\xa0')) <= 1 else infos[0].text.split('|\xa0\xa0')[1]
+        scale_string = infos[1].text.split('：')[1]
+        tmp1 = scale_string.split('（')  # ["40.60亿元", "2018-12-31）"] 这种东西
+        fund_unit = re.findall(r'(亿元|万元)', tmp1[0])[0]
+        fund_scale_lasttime = tmp1[1].split('）')[0]
+        # 防止出现没有数据 -- 的情形
+        fund_scale = '无' if len(re.findall(
+            r'^([0-9]{1,}[.][0-9]*)', tmp1[0])
+        ) == 0 else re.findall(r'^([0-9]{1,}[.][0-9]*)', tmp1[0])[0]
+        fund_manager = infos[2].a.text
+        fund_birthday = infos[3].text.split('：')[1]
+        fund_owener = infos[4].a.text
+        fund_rate = infos[5].div.text
+        detail = {'代码': code, '名称': name, '近1月': m1, '近3月': m3,
+                  '近6月': m6, '近1年': y1, '近3年': y3, '成立来': rece,
+                  '基金类型': fund_type,
+                  '基金风险': fund_risk, '基金规模': fund_scale, '规模单位': fund_unit,
+                  '规模最后更新时间': fund_scale_lasttime, '基金经理': fund_manager,
+                  '成立日': fund_birthday, '管理人': fund_owener, '基金评级': fund_rate}
+        print('detail2', code)
+        col2.insert_one(detail)
+    except:
+        run_detail3(code, name, url, soup)
 
+
+def run_detail1(code, name, url):
+    soup = getUrl.loop_geturl_utf8(url)
+    tags = soup.select('div.dataOfFund dd')
     try:
         m1 = tags[1].select('span')[1].text
         y1 = tags[2].select('span')[1].text
@@ -63,13 +97,20 @@ def run_detail1(code, name, url):
         rece = tags[8].select('span')[1].text
 
         infos = soup.select('div.infoOfFund td')
-        fund_type = infos[0].a.text
-        fund_risk = infos[0].text.split('|\xa0\xa0')[1]
+        fund_type = ''
+        if infos[0].a is None:
+            fund_type = re.findall(r'基金类型：(.+)\xa0\xa0\|', infos[0].text)
+        else:
+            fund_type = infos[0].a.text
+        fund_risk = '无' if len(infos[0].text.split(
+            '|\xa0\xa0')) <= 1 else infos[0].text.split('|\xa0\xa0')[1]
         scale_string = infos[1].text.split('：')[1]
         tmp1 = scale_string.split('（')  # ["40.60亿元", "2018-12-31）"] 这种东西
         fund_unit = re.findall(r'(亿元|万元)', tmp1[0])[0]
         fund_scale_lasttime = tmp1[1].split('）')[0]
-        fund_scale = re.findall(r'^([0-9]{1,}[.][0-9]*)', tmp1[0])[0]
+        # 防止出现没有数据 -- 的情形
+        fund_scale = '无' if len(re.findall(
+            r'^([0-9]{1,}[.][0-9]*)', tmp1[0])) == 0 else re.findall(r'^([0-9]{1,}[.][0-9]*)', tmp1[0])[0]
         fund_manager = infos[2].a.text
         fund_birthday = infos[3].text.split('：')[1]
         fund_owener = infos[4].a.text
@@ -83,23 +124,35 @@ def run_detail1(code, name, url):
         print('detail1', code)
         col2.insert_one(detail)
     except:
-        run_detail2(code, name, url)
+        run_detail2(code, name, url, soup)
 
 
-tags = soup.select('.num_right > li')
-for tag in tags:
-    # tag = tags[0]
-    # print(tag)
-    # if tag:
-    if tag.a is None:
-        continue
-    else:
-        content = tag.a.text  # 取第一个<a>的文本数据
-        code = re.findall(r'\d+', content)[0]  # 拿到基金代码
-        name = content.split('）')[1]  # 拿到股票名称
-        url = tag.a['href']  # 拿到详情链接
-        content_dict = {'code': code, 'name': name, 'url': url}
-        col1.insert_one(content_dict)
-        time.sleep(0.1)
-        run_detail1(code, name, url)
-        print('fund', code)
+for index, fund in enumerate(col1.find()):
+    if index > 7951:
+        print(index, fund['code'])
+        run_detail1(fund['code'], fund['name'], fund['url'])
+        time.sleep(1)
+
+# 下面是爬取基金基本信息的 策略是
+
+# tags = soup.select('.num_right > li')
+# for tag in tags:
+#     # tag = tags[0]
+#     # print(tag)
+#     # if tag:
+#     if tag.a is None:
+#         continue
+#     else:
+#         content = tag.a.text  # 取第一个<a>的文本数据
+#         if not content:
+#             print('hand', tag)
+#             continue
+#         code = re.findall(r'\d+', content)[0]  # 拿到基金代码
+
+#         name = content.split('）')[1]  # 拿到股票名称
+#         url = tag.a['href']  # 拿到详情链接
+#         content_dict = {'code': code, 'name': name, 'url': url}
+#         col1.insert_one(content_dict)
+#         time.sleep(0.1)
+#         # run_detail1(code, name, url)
+#         print('fund', code)
