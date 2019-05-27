9c5255fd162cab11ab72a3de634230d8fff3ae99
Ako520 <jingnanbendan@163.com>
2019-05-27 13:23:10 +0800

    X

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
-import './config.js'
-import { CssBaseline } from '@material-ui/core'
-import AppBar from './app/components/AppBar';
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import Collections from './app/pages/collections/Collections'
+import { CssBaseline } from '@material-ui/core';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MainView from './app/components/MainView';
+import Collections from './app/pages/collections/Collections';
+import WorkingSchedule from './app/pages/WorkingSchedule/workingSchedule';
+import './config.js';
 
 export default class App extends Component {
   render() {
@@ -11,10 +12,10 @@ export default class App extends Component {
       <Router>
         <div>
           <CssBaseline />
-          <AppBar />
+          <MainView />
 
-          {/* <Summary /> */}
           <Route path="/collections" component={Collections} />
+          <Route path="/working-schedule" component={WorkingSchedule} />
         </div>
       </Router>
 
---
+++ src/app/components/MainView.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import AppBar from '../components/AppBar';
+
+class MainView extends Component {
+  render() {
+    return <AppBar />
+  }
+}
+
+export default MainView
